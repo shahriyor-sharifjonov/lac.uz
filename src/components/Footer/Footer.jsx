89dfbe42062f254d3f963ef2054357c16d66364e
Shahriyor <shahriyordeveloping@gmail.com>
2024-03-16 02:56:58 +0500

    init

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,8 @@ import ru from '@/locales/ru'
 import en from '@/locales/en'
 
 import styles from './Footer.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
     const router = useRouter()
@@ -14,7 +16,20 @@ const Footer = () => {
     
     return (
         <footer className={styles.footer}>
-
+            <div className={`${styles.container} container`}>
+                <div className={styles.left}>
+                    <div className={styles.logo}>
+                        <Image src="/logo.svg" width={60} height={80} alt="" draggable={false}></Image>
+                    </div>
+                    <div className={styles.list}>
+                        <Link href="#" className={styles.link}>{t.aboutLink}</Link>
+                        <Link href="#" className={styles.link}>{t.servicesLink}</Link>
+                        <Link href="#" className={styles.link}>{t.callbackLink}</Link>
+                        <Link href="#" className={styles.link}>{t.contactsLink}</Link>
+                    </div>
+                </div>
+                <Link href="https://bit.ly/shahriyorweb" target="_blank" className={`${styles.link} ${styles.copy}`}>Разработка сайтов</Link>
+            </div>
         </footer>
     )
 }
